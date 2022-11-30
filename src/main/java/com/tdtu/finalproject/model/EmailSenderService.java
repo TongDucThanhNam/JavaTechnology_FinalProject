a43f61cb31e9edd1a6209615dbfab7677c230f8e
DatTran271 <95118734+DatTran271@users.noreply.github.com>
2022-12-01 00:16:39 +0700

    Service để gửi email
    Phần này để test được chức năng @EventListener

---
+++ src/main/java/com/tdtu/finalproject/model/EmailSenderService.java
@@ -0,0 +1,20 @@
+package com.tdtu.finalproject.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+	@Autowired
+	private JavaMailSender mailSender;
+	public void sendEmail(String toEmail,String subject,String message) {
+		SimpleMailMessage msg=new SimpleMailMessage();
+		msg.setFrom("thiendat111@gmail.com");
+		msg.setTo(toEmail);
+		msg.setText(message);
+		msg.setSubject(subject);
+		mailSender.send(msg);
+	}
+}
